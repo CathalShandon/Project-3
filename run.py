99795c81b24e40ad3d41abccda168850d37580fd
CathalShandon <Cathalshandon@gmail.com>
2022-05-10 15:27:35 +0000

    Add initial start to game

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import time
+
+#Initial steps to start game
+print("Welcome to the Hangman game")
+name = input("Enter your name: ")
+print("Hello " + name + " Best of luck in this Hangman game!" )
+
+#The game
+def main():
+    global count
+    
