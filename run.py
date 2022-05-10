e039d0e09b951e54911c9f90a1de9d36013dbeb1
CathalShandon <Cathalshandon@gmail.com>
2022-05-10 16:05:59 +0000

    Add code for main game

--- run.py
+++ run.py
@@ -9,4 +9,16 @@ print("Hello " + name + " Best of luck in this Hangman game!" )
 #The game
 def main():
     global count
-    
+    global display
+    global word
+    global already_guessed
+    global length
+    global play_game
+    words_to_guess = &#91; "Chelsea", "Liverpool", "Burnley", "Watford", "Everton", "Arsenal", "Wolves", "Tottenham", "Brentford", "Newcastle"]
+    word = random.choice(words_to_guess) 
+    length = len(word)
+    count = 0
+    display = ' ' * length
+    already_guessed = &#91;]
+    play_game = ''
+ 
