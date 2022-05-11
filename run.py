99d90b9dce951b29c83111ee518072d75a2c7bef
CathalShandon <Cathalshandon@gmail.com>
2022-05-11 11:33:08 +0000

    Add conditions required to play game

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import random
 import time
 
-#Initial steps to start game
+# Initial steps to start game #
 print("Welcome to the Hangman game")
 name = input("Enter your name: ")
 print("Hello " + name + " Best of luck in this Hangman game!" )
 
-#The game
+# The game #
 def main():
     global count
     global display
@@ -14,11 +14,38 @@ def main():
     global already_guessed
     global length
     global play_game
-    words_to_guess = &#91; "Chelsea", "Liverpool", "Burnley", "Watford", "Everton", "Arsenal", "Wolves", "Tottenham", "Brentford", "Newcastle"]
+    words_to_guess = ["Chelsea", "Liverpool", "Burnley", "Watford", "Everton", "Arsenal", "Wolves", "Tottenham", "Brentford", "Newcastle"]
     word = random.choice(words_to_guess) 
     length = len(word)
     count = 0
     display = ' ' * length
-    already_guessed = &#91;]
+    already_guessed = []
     play_game = ''
  
+# A loop to re-excute when the first rounds ends #
+
+def play_loop():
+    global play_game
+    play_game = input("Do You want to play game again? y = yes, n = no \n ")
+    while play_game not in ["y", "n", "Y", "N"]
+        play_game input("Do You want to play game again? y = yes, n = no \n ")
+    if play_game == 'y':
+        main()
+    elif play_game == "n":
+        print("Thanks For Playing! We expect you back again!")
+        exit()
+
+# Conditions required to play game #
+def Hangman():
+    global count 
+    global display
+    global word
+    global already_guessed
+    global play_game 
+    limit = 5 
+    guess = input("This is the Hangman word: " + display + "Enter your guess: \n")
+    guess = guess.strip()
+    if len(guess.strip()) == 0 or len(guess.strip()) == 2 or guess == 9:
+        print("Invalid Input, Try a letter\n")
+        hangman()
+
